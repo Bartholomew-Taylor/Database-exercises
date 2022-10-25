833e4db58064a857d5863e956f7325e9fce6f601
Bart Taylor <bartholomew.s.taylor@gmail.com>
2022-10-25 12:30:02 -0500

    updated and finished exercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,2 +1,28 @@
 USE employees;
 
+/* Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
+MANG ERIE
+EVGUENI SRIMANI
+FLORINA GARNIER
+SORINA KERMARREC
+CHRISS BINDING
+*/
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date like '%12-25'
+LIMIT 5;
+
+/* Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results
+*/
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date like '%12-25'
+LIMIT 5 OFFSET 45;
+
+/* LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
+OFFSET removes the first chunk and then limit will show the next set, offset divided by limit will give you the number of pages before the current page
+*/
+
+
