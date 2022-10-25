c6554742de15e2e57f91edae73f3e257b8ca2bf5
Bart Taylor <bartholomew.s.taylor@gmail.com>
2022-10-25 14:31:07 -0500

    limit exercise updated again

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -8,16 +8,17 @@ ORDER BY last_name DESC
 LIMIT 10;
 
 /* Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
-MANG ERIE
-EVGUENI SRIMANI
-FLORINA GARNIER
-SORINA KERMARREC
-CHRISS BINDING
+ALSELM CAPPELLO
+UTZ MANDELL
+BOUCHUNG SCHREITER
+BAOCAI KUSHNER
+PETTER STROUSTRUP
 */
 
 SELECT *
 FROM employees
 WHERE hire_date LIKE '199%' AND birth_date like '%12-25'
+ORDER BY hire_date ASC
 LIMIT 5;
 
 /* Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results
