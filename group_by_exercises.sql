32730b4ccb964b125381f234ac7549fed939d703
Bart Taylor <bartholomew.s.taylor@gmail.com>
2022-10-26 14:30:17 -0500

    changes made to group by exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -11,7 +11,7 @@ FROM titles;
 SELECT last_name
 FROM employees
 GROUP BY last_name
-HAVING last_name like '%e%';
+HAVING last_name like 'e%e';
 
 #4. Write a query to to find all unique combinations of first and last names of all employees whose last names start and end with 'E'.
 
@@ -38,21 +38,23 @@ HAVING last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
 
 #7 Find all all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
 
-SELECT first_name, COUNT(gender)
+SELECT first_name, gender, COUNT(*) AS totalcount
 FROM employees
-WHERE first_name IN('irena', 'vidya', 'maya') AND gender = 'm'
-GROUP BY first_name;
-# for gender M
-SELECT first_name, COUNT(gender)
-FROM employees
-WHERE first_name IN('irena', 'vidya', 'maya') AND gender = 'f'
-GROUP BY first_name;
-#for gedner F
+GROUP BY first_name, gender
+HAVING first_name IN('irena', 'vidya', 'maya')
+ORDER BY first_name;
+
 
 # 8 Using your query that generates a username for all of the employees, generate a count employees for each unique username. Are there any duplicate usernames? BONUS: How many duplicate usernames are there?
 
-SELECT CONCAT(SUBSTR(first_name, 1,1),SUBSTR(last_name,1,4),'_', SUBSTR(birth_date,6,2), SUBSTR(birth_date,3,2)) AS username, count(CONCAT(SUBSTR(first_name, 1,1),SUBSTR(last_name,1,4),'_', SUBSTR(birth_date,6,2), SUBSTR(birth_date,3,2))) AS username_count
-from employees
+SELECT CONCAT(SUBSTR(first_name, 1,1),SUBSTR(last_name,1,4),'_', SUBSTR(birth_date,6,2), SUBSTR(birth_date,3,2)) AS username, count(*) AS username_count
+FROM employees
 GROUP BY username
-HAVING username_count > 1;
+HAVING username_count > 1 #this is the bonus part
+ORDER BY username_count desc; 
 #yes there are 13,251 usernames used more than once
+
+SELECT emp_no, avg(salary)
+FROM salaries
+GROUP BY emp_no;
+
