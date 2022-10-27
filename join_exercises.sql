d86abe15eba8687dfbb104507c6eac6f27f0bdda
Bart Taylor <bartholomew.s.taylor@gmail.com>
2022-10-26 22:54:33 -0500

    join exercise updated

--- join_exercises.sql
+++ join_exercises.sql
@@ -7,3 +7,32 @@ from departments
 Join dept_manager USING(dept_no)
 Join employees ON employees.emp_no = dept_manager.emp_no
 WHERE dept_manager.to_date LIKE '9999%';
+
+# 3 Find the name of all departments currently managed by women.
+
+SELECT departments.dept_name, CONCAT(employees.first_name, ' ', employees.last_name)
+from departments
+Join dept_manager USING(dept_no)
+Join employees ON employees.emp_no = dept_manager.emp_no
+WHERE dept_manager.to_date LIKE '9999%' AND employees.gender = 'f';
+
+#4 Find the current titles of employees currently working in the Customer Service department.
+
+SELECT departments.dept_name, CONCAT(employees.first_name, ' ', employees.last_name)
+from departments
+Join dept_emp USING(dept_no)
+Join employees ON employees.emp_no = dept_emp.emp_no
+WHERE departments.dept_name = "Customer Service" AND dept_emp.to_date LIKE "9999%";
+
+#5 Find the current salary of all current managers.
+
+SELECT departments.dept_name AS department, CONCAT(employees.first_name, ' ', employees.last_name) AS managers, salaries.salary AS salary
+from departments
+Join dept_manager USING(dept_no)
+JOIN salaries ON salaries.emp_no = dept_manager.emp_no
+Join employees ON employees.emp_no = dept_manager.emp_no
+WHERE dept_manager.to_date LIKE '9999%'
+GROUP BY department, managers, salary;
+
+# solve repeat salary issue
+
