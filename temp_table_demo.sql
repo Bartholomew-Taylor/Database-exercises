1f0737ef4f172b4267c59d30c68b8d95eafaa65a
Bart Taylor <bartholomew.s.taylor@gmail.com>
2022-10-31 15:05:43 -0500

    temp table demo and exercises updated

--- temp_table_demo.sql
+++ temp_table_demo.sql
@@ -12,3 +12,74 @@ CREATE TEMPORARY TABLE my_numbers(
 
 SELECT *
 FROM my_numbers;
+#inserting data
+INSERT INTO my_numbers(n, NAME)
+VALUES (1, 'a'), (2, 'b'), (3, 'c');
+
+#delete from my_numbers
+DELETE FROM my_numbers WHERE n < 2;
+
+#transform data
+
+UPDATE my_numbers SET n = n + 1;
+
+
+#transform existing data w/ condition
+
+UPDATE my_numbers SET n = n + 10
+WHERE name = 'a';
+
+SELECT *
+FROM my_numbers;
+
+USE employees;
+
+SELECT emp_no, dept_no, first_name, last_name, salary, title
+FROM employees AS e
+JOIN dept_emp AS de USING(emp_no)
+JOIN salaries AS s USING(emp_no)
+JOIN departments AS d USING(dept_no)
+JOIN titles AS t using(emp_no)
+WHERE de.to_date > NOW()
+     AND s.to_date > NOW()
+     AND t.to_date > NOW()
+     AND dept_name = 'customer service';
+     
+CREATE TEMPORARY TABLE noether_2022.salary_info AS (
+
+SELECT emp_no, dept_no, first_name, last_name, salary, title
+FROM employees AS e
+JOIN dept_emp AS de USING(emp_no)
+JOIN salaries AS s USING(emp_no)
+JOIN departments AS d USING(dept_no)
+JOIN titles AS t using(emp_no)
+WHERE de.to_date > NOW()
+     AND s.to_date > NOW()
+     AND t.to_date > NOW()
+     AND dept_name = 'customer service'
+     
+);
+
+SELECT *
+FROM noether_2022.salary_info;
+
+
+#avg salary for current employees
+
+SELECT AVG(salary)
+FROM noether_2022.salary_info;
+
+#adding new columns
+ALTER TABLE noether_2022.salary_info ADD avg_salary FLOAT;
+ALTER TABLE noether_2022.salary_info ADD std_salary FLOAT;
+ALTER TABLE noether_2022.salary_info ADD greater_than_avg_salary INT;
+
+SELECT *
+FROM noether_2022.salary_info;
+
+#update new columns
+UPDATE noether_2022.salary_info SET avg_salary = 67000;
+
+UPDATE noether_2022.salary_info SET std_salary = 16000;
+
+UPDATE noether_2022.salary_info SET greater_than_avg_salary = salary > avg_salary;
---
+++ temp_table_exercises.sql
@@ -0,0 +1,69 @@
+#1 Using the example from the lesson, create a temporary table called employees_with_departments that contains first_name, last_name, and dept_name for employees currently with that department. Be absolutely sure to create this table on your own database. If you see "Access denied for user ...", it means that the query was attempting to write a new table to a database that you can only read.
+     
+CREATE TEMPORARY TABLE noether_2022.employees_with_departments AS (
+
+SELECT first_name, last_name, title, dept_name
+FROM employees AS e
+JOIN dept_emp AS de USING(emp_no)
+JOIN departments AS d USING(dept_no)
+JOIN titles AS t using(emp_no)
+WHERE de.to_date > NOW()
+     AND t.to_date > NOW()
+     AND dept_name = 'customer service'
+     
+);
+
+SELECT * 
+FROM noether_2022.employees_with_departments;
+
+#1.a Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+
+ALTER TABLE noether_2022.employees_with_departments ADD full_name VARCHAR(30);
+
+#1b Update the table so that full name column contains the correct data
+
+UPDATE employees_with_departments SET 
+full_name = CONCAT(first_name, ' ', last_name);
+
+#1.c Remove the first_name and last_name columns from the table.
+
+ALTER TABLE employees_with_departments DROP COLUMN last_name;
+ALTER TABLE employees_with_departments DROP COLUMN first_name;
+
+#1d we could've just created it to with those columns to being with?
+
+DROP TABLE IF EXISTS noether_2022.payments_copy;
+
+/* 2. Create a temporary table based on the payment table from the sakila database.
+
+Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199. */
+
+CREATE TEMPORARY TABLE noether_2022.payments_copy AS (
+						select *
+							 from sakila.payment AS s_p	
+							 		 );
+select *
+FROM noether_2022.payments_copy;
+
+
+ALTER TABLE noether_2022.payments_copy ADD amount_in_cents INT;
+UPDATE noether_2022.payments_copy SET amount_in_cents = amount * 100;
+
+ALTER TABLE noether_2022.payments_copy DROP COLUMN amount;
+
+# 3 Find out how the current average pay in each department compares to the overall current pay for everyone at the company. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst?
+
+DROP TABLE IF EXISTS noether_2022.ind_pay_vs_dept_pay;
+
+CREATE TEMPORARY TABLE noether_2022.ind_pay_vs_dept_pay as (
+						SELECT CONCAT(employees.first_name,' ', employees.last_name),
+										employees.emp_no, salaries.salary, departments.dept_name
+							 FROM employees.employees
+							 JOIN dept_emp USING(emp_no)
+							 JOIN salaries USING(emp_no)							 JOIN departments USING(dept_no) 
+							 WHERE salaries.to_date > CURDATE()
+							);
+							
+Select *
+FROM noether_2022.ind_pay_vs_dept_pay;
+
